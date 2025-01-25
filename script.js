5c0375912f5e42c529c4b46023ea41b3a3f3a1be
Marionaaa22 <mariona.clars@mataro.epiaedu.cat>
2025-01-26 00:22:30 +0100

    Maquetacion

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    menu.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function() {
+    const elements = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                entry.target.classList.remove('fade-out');
+            } else {
+                entry.target.classList.remove('visible');
+                entry.target.classList.add('fade-out');
+            }
+        });
+    }, {
+        threshold: 0.1 // La cantidad de visibilidad que debe tener el elemento para activar la animación
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
 });
